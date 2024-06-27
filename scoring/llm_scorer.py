27887235744efdffcee15d92a26051c792cb3f2d
prishasamdarshi <91812830+prishasamdarshi@users.noreply.github.com>
2024-06-27 14:22:34 -0700

    Update llm_scorer.py

--- scoring/llm_scorer.py
+++ scoring/llm_scorer.py
@@ -7,8 +7,8 @@ This script scores Connections answers from the LLM responses.
 *   Parses LLM responses
 *   Scores groupings for accuracy according to the provided scoring schema.
 
-Assumes input CSV file: `gemini_responses.csv` or `claude_responses.csv`
-Outputs CSV file with classification score in a new column
+Assumes input CSV file: `gemini_responses.csv`, `claude_responses.csv`,
+Outputs 4 CSV files for each LLM with parsed responses and clustering score in new columns
 Dependencies: `pandas`
 '''
 
@@ -42,7 +42,7 @@ def split_and_reshape(game):
     split_list = game.split(', ')
     return [split_list[i:i+4] for i in range(0, len(split_list), 4)]
 
-#%%
+
 def clean_response(response):
     """Clean the LLM responses using brackets [] as delimiters
     Parameters: String of llm responses (one cell in responses df)
@@ -73,13 +73,13 @@ def clean_response(response):
 
 
 def score_response(row: pd.Series):
-    """Calculates classification score of responses according to our scoring schema
-    * Scoring Schema *
+    """Calculates Clustering scores of responses according to our scoring schema
+    * Scoring Schema Weighted Clustering Score *
     - Yellow Category (+1), Green Category (+2), Blue Category (+3), Purple Category (+4)
-    - Maximum Classification Score = 10, Minimum Classification Score = 0
-    - Score of 99 means calc this score manually
+    - Maximum Clustering Score = 10, Minimum Clustering Score = 0
+    - Score of 99 means calculate this score manually
     Parameters: Parsed LLM response
-    Returns: Classification score
+    Returns: Clustering scores
     """
     response = row["Parsed Response"]
     # Each answer category is in a different column
@@ -87,31 +87,36 @@ def score_response(row: pd.Series):
     answer_green = row["Green"]
     answer_blue = row["Blue"]
     answer_purple = row["Purple"]
-    # Initialize score of game
-    score = 0
+    # Initialize scores of game
+    weighted_cscore = 0
+    c_score = 0
     for a in response:
         # Scoring mechanism
         try:
             if set(a) == set(answer_yellow):
-                score += 1
+                weighted_cscore += 1
+                c_score += 1
             if set(a) == set(answer_green):
-                score += 2
+                weighted_cscore += 2
+                c_score += 1
             if set(a) == set(answer_blue):
-                score += 3
+                weighted_cscore += 3
+                c_score += 1
             if set(a) == set(answer_purple):
-                score += 4
+                weighted_cscore += 4
+                c_score += 1
         except IndexError:
             # If error, flag to score manually and continue
-            score = 99
+            weighted_cscore = 99
             continue
-    return score
-
+    return pd.Series({ "Weighted Clustering Score": weighted_cscore,
+                       "Unweighted Clustering Score": c_score})
 
 
 def create_scored_df(answers_df, responses_file):
     """Parse and score llm responses
     Parameters: df returned by parse_answers(), path to llm response file
-    Returns: df with original response, parsed response, and classification score columns
+    Returns: df with original response, parsed response, and clustering score columns
     """
     # Read and prepare responses_file
     responses_df = pd.read_csv(responses_file, header=None)
@@ -122,7 +127,7 @@ def create_scored_df(answers_df, responses_file):
     # Join answers_df to responses df
     responses_df = responses_df.join(answers_df, how="left", rsuffix="_answers")
     # Calculate scores
-    responses_df["Classification Score"] = responses_df.apply(score_response, axis=1)
+    responses_df[["Weighted Clustering Score", "Unweighted Clustering Score"]] = responses_df.apply(score_response, axis=1)
     # Drop answers columns
     responses_df = responses_df.drop(["Yellow", "Green", "Blue", "Purple"], axis=1)
 
@@ -130,11 +135,16 @@ def create_scored_df(answers_df, responses_file):
 
 
 if __name__ == '__main__':
-    answers_df = parse_answers("connectionsRes.csv")
-    # Remove comments to run
+    answers_df = parse_answers("reasoning/connectionsRes.csv")
     # Gemini
-    #gemini_scored_responses_df = create_scored_df(answers_df, "gemini_responses.csv")
-    #gemini_scored_responses_df.to_csv("gemini_scored.csv", index=False)
+    gemini_scored_responses_df = create_scored_df(answers_df, "responses/gemini_responses.csv")
+    gemini_scored_responses_df.to_csv("gemini_scored.csv", index=False)
     # Claude
-    #claude_scored_responses_df = create_scored_df(answers_df, "claude_responses.csv")
-    #claude_scored_responses_df.to_csv("claude_scored.csv", index=False)
+    claude_scored_responses_df = create_scored_df(answers_df, "responses/claude_responses.csv")
+    claude_scored_responses_df.to_csv("claude_scored.csv", index=False)
+    # GPT4-o
+    chatgpt_scored_responses_df = create_scored_df(answers_df, "responses/chatgpt_responses.csv")
+    chatgpt_scored_responses_df.to_csv("gpt_scored.csv", index=False)
+    # Llama
+    llama_scored_responses_df = create_scored_df(answers_df, "responses/llama_responses.csv")
+    llama_scored_responses_df.to_csv("llama_scored.csv", index=False)
